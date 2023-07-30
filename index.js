4291121cc7194a01e061e59f707a6dadbe511a9f
Gabriel Fernandez <gfernandezareas@gmail.com>
2023-07-30 10:34:04 +0200

    from local

--- index.js
+++ index.js
@@ -21,12 +21,43 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp", (req, res) => {
+  let actualTime = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  });
+});
 
+app.get("/api/timestamp/:date_string", (req, res) => {
+  
+  let dateStr = req.params.date_string;
+
+  if(parseInt(dateStr) > 10000) {
+    let unixTime = new Date(parseInt(dateStr));
+    res.json({
+      "unix": unixTime.getTime(),
+      "utc": unixTime.toUTCString()
+    });
+  }
+
+  let passedInValue = new Date(dateStr);
+
+  if(passedInValue === "Invalid Date") {
+    res.json({ "error": "Invalid Date" });
+  } else {
+    res.json({
+      "unix": passedInValue.getTime(),
+      "utc": passedInValue.toUTCString()
+    });
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
