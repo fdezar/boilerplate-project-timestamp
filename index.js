6df8299042b2423828ccb81e31f9699a6d211352
Replit user <>
2023-08-19 20:08:38 +0000

    Done

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
-// index.js
-// where your node app starts
-
-// init project
+// setup
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// FCC Test
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200})); 
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/:date", (req, res) => {
+  let date = new Date(req.params.date);
 
-  res.json({greeting: 'hello API'});
-});
+  if(isInvalidDate(date)) {
+    date = new Date(+req.params.date); 
+  }
 
-app.get("/api/timestamp", (req, res) => {
-  let actualTime = new Date();
+  if(isInvalidDate(date)) {
+    res.json({
+      error: "Invalid Date"
+    });
+    return;
+  }
+
   res.json({
-    "unix": now.getTime(),
-    "utc": now.toUTCString()
+    unix: date.getTime(),
+    utc: date.toUTCString()
   });
 });
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  
-  let dateStr = req.params.date_string;
-
-  if(parseInt(dateStr) > 10000) {
-    let unixTime = new Date(parseInt(dateStr));
-    res.json({
-      "unix": unixTime.getTime(),
-      "utc": unixTime.toUTCString()
-    });
-  }
-
-  let passedInValue = new Date(dateStr);
-
-  if(passedInValue === "Invalid Date") {
-    res.json({ "error": "Invalid Date" });
-  } else {
-    res.json({
-      "unix": passedInValue.getTime(),
-      "utc": passedInValue.toUTCString()
-    });
-  }
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
 });
+  
 
-// listen for requests :)
+// port 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// http://expressjs.com/en/starter/static-files.html
+// http://expressjs.com/en/starter/basic-routing.html
